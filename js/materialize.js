f28906462841da3f0887388739198829e14e2ab1
Jcarlos-rl <carlos_-619@hotmail.com>
2019-05-12 15:11:07 -0500

    Inicio

---
+++ js/materialize.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+
+$(document).ready(function(){
+    $('.modal').modal();
+});
+
+$(document).ready(function(){
+    $('select').formSelect();
+});
+
+$(document).ready(function(){
+    $('.tooltipped').tooltip();
+});
+
+$(document).ready(function(){
+    $('.carousel').carousel({
+        dist: -10
+    });
+});
